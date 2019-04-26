fc5d61722ea741999445a6f2fe1e0a061b503559
jabusir <jabusir@gmail.com>
2019-04-25 23:32:17 -0700

    Updates

---
+++ precache-manifest.949090d8cefd43fc7e42749960e1acdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081f555e7b30e9652c646d1f02d2a3a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b08781af462cac2a812c",
+    "url": "/static/css/main.488bc2a6.chunk.css"
+  },
+  {
+    "revision": "503584ae81f6eedaa620",
+    "url": "/static/js/2.83ff8301.chunk.js"
+  },
+  {
+    "revision": "b08781af462cac2a812c",
+    "url": "/static/js/main.7fb5d8ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
